a30686f51ec32e838e826160880eb62c3cdb8251
rdilimas <rdilimas@gmail.com>
2024-09-22 10:01:23 -0300

    docs: Add exemplo operadores aritimeticos

---
+++ programas/operadores_aritmeticos.py
@@ -0,0 +1,30 @@
+
+
+produto_1 = 20
+produto_2 = 10
+
+
+print(produto_1 + produto_2)
+
+print(produto_1 - produto_2)
+
+print(produto_1 / produto_2)
+
+#Divisão com resultado inteiro
+print(produto_1 // produto_2)
+
+print(produto_1 * produto_2)
+
+#modulo ou resto da divisao
+print(produto_1 % produto_2)
+
+#exponenciacao
+print(produto_1 ** produto_2)
+
+#respeita a precedencia
+x = 10 + 5 * 4
+print(x)
+
+#é interessante deixar a ordem explicita
+x = (10 + 5) * 4
+print(x)
