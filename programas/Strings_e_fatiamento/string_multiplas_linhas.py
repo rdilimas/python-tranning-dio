b018eddd0d3a5241cb76a228ed7146a3ff61b97e
rdilimas <rdilimas@gmail.com>
2024-09-23 01:43:16 -0300

    feat: add fonte aula string com muitas linhas

---
+++ programas/Strings_e_fatiamento/string_multiplas_linhas.py
@@ -0,0 +1,15 @@
+
+nome = "Robson"
+
+mensagem = f"""
+    Olá, meu nome é {nome}
+ Eu estou aprendendo Python.
+    Essa mensagem tem diferents recups
+"""    
+
+print(mensagem)
+
+
+
+texto = " Python ".lstrip()
+print(texto)
