a9315e89546ef737cf33f0f2ecc0b770a290c633
rdilimas <rdilimas@gmail.com>
2024-10-11 00:21:30 -0300

    feat: add exemplos Atributos de classe ou instancias e Métodos de classe estático

---
+++ programas/Programação Orientada a Objetos/Atributos de classe ou instância/atributos_classe_instancia.py
@@ -0,0 +1,26 @@
+class Estudante:
+    escola = "DIO"
+
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+    def __str__(self) -> str:
+        return f"{self.nome} - {self.matricula} - {self.escola}"
+
+
+def mostrar_valores(*objs):
+    for obj in objs:
+        print(obj)
+
+
+aluno_1 = Estudante("Guilherme", 1)
+aluno_2 = Estudante("Giovanna", 2)
+mostrar_valores(aluno_1, aluno_2)
+
+#Variavel de classe, comum a todos, 
+Estudante.escola = "Python"
+
+#variavel de instancia, cada um tem a sua
+aluno_3 = Estudante("Chappie", 3)
+mostrar_valores(aluno_1, aluno_2, aluno_3)
